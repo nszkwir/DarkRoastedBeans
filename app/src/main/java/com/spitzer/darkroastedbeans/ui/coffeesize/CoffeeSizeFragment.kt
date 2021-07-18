ff90e25da643f1206d9d23eccb98ca136e34a90d
Nicolas Szkwir <nicoszkwir@gmail.com>
2021-07-18 17:57:53 -0300

    implemented CoffeeSizeFragment

--- app/src/main/java/com/spitzer/darkroastedbeans/ui/coffeesize/CoffeeSizeFragment.kt
+++ app/src/main/java/com/spitzer/darkroastedbeans/ui/coffeesize/CoffeeSizeFragment.kt
@@ -6,17 +6,31 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.spitzer.darkroastedbeans.R
 import com.spitzer.darkroastedbeans.core.BaseFragment
 import com.spitzer.darkroastedbeans.databinding.CoffeeSizeFragmentBinding
 import com.spitzer.darkroastedbeans.databinding.CoffeeStyleFragmentBinding
+import com.spitzer.darkroastedbeans.model.CoffeeSelectionModel
+import com.spitzer.darkroastedbeans.ui.coffeestyle.CoffeeStyleFragmentArgs
+import com.spitzer.darkroastedbeans.ui.coffeestyle.CoffeeStyleFragmentDirections
 import com.spitzer.darkroastedbeans.ui.machinepairing.MachinePairingFragmentViewModel
+import com.spitzer.darkroastedbeans.uicomponents.expandablecoffeeitem.adapters.CoffeeItemAdapter
 
 class CoffeeSizeFragment : BaseFragment() {
+
     private var _binding: CoffeeSizeFragmentBinding? = null
     private val binding get() = _binding!!
-    private lateinit var viewModel: CoffeeSizeFragmentViewModel
 
+    private lateinit var viewModel: CoffeeSizeFragmentViewModel
     override fun getViewModel() = viewModel
+
+    private lateinit var coffeeItemAdapter: CoffeeItemAdapter
+    private var model: CoffeeSelectionModel? = null
+    val args: CoffeeSizeFragmentArgs by navArgs()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,17 +47,46 @@ class CoffeeSizeFragment : BaseFragment() {
         savedInstanceState: Bundle?
     ): View {
         _binding = CoffeeSizeFragmentBinding.inflate(inflater, container, false)
-        setupView()
-        defineObservables()
         return binding.root
     }
 
-    fun defineObservables() {
-        //findNavController().navigate()
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        model = args.model
+
+        if (model == null || model?.machineConfiguration == null) {
+            findNavController().popBackStack(R.id.MachinePairingFragment, false)
+        }
+
+        coffeeItemAdapter = CoffeeItemAdapter(
+            (model as CoffeeSelectionModel).getSizesByStyle(),
+            { headerId -> onHeaderClick(headerId) },
+            { headerId, extraId -> onExtrasClick(headerId, extraId) })
+
+        binding.coffeeSizesRecyclerView.apply {
+            layoutManager =
+                LinearLayoutManager(context, RecyclerView.VERTICAL, false)
+            adapter = coffeeItemAdapter
+            adapter?.notifyDataSetChanged()
+        }
     }
 
-    fun setupView() {
+    private fun onHeaderClick(headerId: String) {
+        model?.let {
+            if (headerId.isNotEmpty()) {
+                model?.sizeId = headerId
+                val action = CoffeeSizeFragmentDirections
+                    .actionCoffeeSizeFragmentToCoffeeExtrasFragment(model!!)
+                findNavController().navigate(action)
+            }
+        }
+        //showSnackBar("HeaderId: $headerId")
+    }
 
+    private fun onExtrasClick(headerId: String, extraId: String) {
+        // showSnackBar("HeaderId: $headerId - ExtraId: $extraId")
+        // This function should not be executed
+        // The styles shown has no extras
     }
 
     override fun onDestroyView() {
